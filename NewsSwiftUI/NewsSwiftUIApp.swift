aceef9c0f5bc01776122049439982932593aa6a4
SonataGirl <mike@magic.org>
2023-06-21 22:31:36 +0300

    Initial Commit

---
+++ NewsSwiftUI/NewsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsSwiftUIApp.swift
+//  NewsSwiftUI
+//
+//  Created by Sonata Girl on 21.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
